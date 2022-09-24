d1425363e5adf363135015885a76713bfdeea9c7
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2022-09-24 19:39:33 +0100

    Add error to all except email

--- val.js
+++ val.js
@@ -1,11 +1,26 @@
 const nam = document.getElementById("name");
+const last = document.getElementById("last");
 const form = document.getElementById('form');
+const password = document.getElementById("password");
+const email = document.getElementById("email");
+
+
 
 form.addEventListener('submit', (e) => {
     if (nam.value == '' || nam.value == null) {
         nam.classList.add("java");
         e.preventDefault();
     }
+    if (last.value == '' || last.value == null) {
+        last.classList.add("java");
+        e.preventDefault();
+    }
+    if (password.value == '' || password.value == null || password.value == 'Password' || password.value == 'password') {
+        password.classList.add("java");
+        e.preventDefault();
+    }
+    
+  
 })
 
 
