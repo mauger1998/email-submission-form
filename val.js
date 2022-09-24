f6e8b7d13924dbd68b4984aac2bf6ae1937f045a
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2022-09-24 19:47:17 +0100

    Add error msg to html under inputs write script to apply when wrong

--- val.js
+++ val.js
@@ -3,22 +3,29 @@ const last = document.getElementById("last");
 const form = document.getElementById('form');
 const password = document.getElementById("password");
 const email = document.getElementById("email");
+const error1 = document.getElementById("first-error");
+const error2 = document.getElementById("second-error");
+const error4 = document.getElementById("fourth-error");
 
 
 
 form.addEventListener('submit', (e) => {
     if (nam.value == '' || nam.value == null) {
         nam.classList.add("java");
+        error1.classList.add("appear");
         e.preventDefault();
     }
     if (last.value == '' || last.value == null) {
         last.classList.add("java");
+        error2.classList.add("appear");
         e.preventDefault();
     }
     if (password.value == '' || password.value == null || password.value == 'Password' || password.value == 'password') {
         password.classList.add("java");
+        error4.classList.add("appear");
         e.preventDefault();
     }
+    
     
   
 })
